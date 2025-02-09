c19485199d18015867b8fc17dde1f00e56dcc271
t-drape <thebatreview1@gmail.com>
2025-02-08 21:21:07 -0700

    Add home button redirect to in go_home function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,6 +41,10 @@ class ArticlesController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
+  def go_home
+    redirect_to root_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
