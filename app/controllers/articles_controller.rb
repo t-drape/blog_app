730ecfa330967fdff85d6f4819bdd4d0e06c73b1
t-drape <thebatreview1@gmail.com>
2025-02-08 21:33:55 -0700

    Remove home funciton

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,10 +35,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def home
-    redirect_to root_path
-  end
-
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
